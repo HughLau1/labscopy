e43f0136c6f5200142c8f436f9d8307c937edcf8
Hugh Lau <hughxlau@gmail.com>
2021-06-29 20:18:11 -0700

    finished

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,11 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node *slowPtr = head, *fastPtr = head;
+    while(fastPtr && fastPtr -> next){
+	slowPtr = slowPtr->next;
+	fastPtr = fastPtr -> next -> next;
+	if (slowPtr == fastPtr) return 1;
+    }
+     return 0;  
 }
